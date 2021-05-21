672171be57f98519dcd9f7bdcfbcc208e1eb95be
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-05-21 21:19:10 +0200

    ToolBox: initial commit

---
+++ cmd/root.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "tb",
+	Short: "ToolBox useful command line tools",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "ToolBox version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("ToolBox version 0.0.1")
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"tb/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
