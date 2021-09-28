208f8092deb6d9cb7c4d500315fbb4dd09409032
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-09-28 23:18:28 +0200

    ToolBox: adding crypto adding sha256 hash function

--- cmd/crypto.go
+++ cmd/crypto.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -10,8 +11,10 @@ import (
 func init() {
 	cryptoCmd.AddCommand(
 		cryptoMD5Cmd,
+		cryptoSHA256Cmd,
 	)
 }
+
 var cryptoCmd = &cobra.Command{
 	Use:   "crypto",
 	Short: "crypto package utilities",
@@ -34,5 +37,21 @@ var cryptoMD5Cmd = &cobra.Command{
 
 		text := []byte(args[0])
 		fmt.Printf("%x\n", md5.Sum(text))
+	},
+}
+
+var cryptoSHA256Cmd = &cobra.Command{
+	Use:     "sha256",
+	Example: "sha256 example_text",
+	Short:   "sha256 hash function on first argument",
+	Long:    "sha256 hash function on first argument",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			cmd.Help()
+			return
+		}
+
+		text := []byte(args[0])
+		fmt.Printf("%x\n", sha256.Sum256(text))
 	},
 }
