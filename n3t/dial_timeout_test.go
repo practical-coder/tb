07f523b8ab0ae5aa5b51a275432accfa4d9e9112
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-10-13 21:47:26 +0200

    Dialer DialContext: same purpose as custom DialTimeout, but more flexible

--- n3t/dial_timeout_test.go
+++ n3t/dial_timeout_test.go
@@ -1,7 +1,9 @@
 package n3t
 
 import (
+	"context"
 	"net"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -21,5 +23,35 @@ func TestDialTimeout(t *testing.T) {
 	if !netErr.Timeout() {
 		t.Log("Some other error, not timeout!")
 		t.Fatal(err)
+	}
+}
+
+func TestDialContext(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var dialer net.Dialer
+	dialer.Control = func(_, _ string, _ syscall.RawConn) error {
+		time.Sleep(3*time.Second + time.Millisecond)
+		return nil
+	}
+
+	conn, err := dialer.DialContext(ctx, "tcp", "10.7.7.7:http")
+	if err == nil {
+		conn.Close()
+		t.Fatal("Connection did not timed out")
+	}
+
+	netErr, ok := err.(net.Error)
+	if !ok {
+		t.Error(err)
+	} else {
+		if !netErr.Timeout() {
+			t.Errorf("Error is not timeout: %v", err)
+		}
+	}
+
+	if ctx.Err() != context.DeadlineExceeded {
+		t.Errorf("DeadlineExceeded expected! Got: %v", ctx.Err())
 	}
 }
