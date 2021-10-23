d5d353fb725ceec53b7c830ece077924196f968d
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-10-23 13:39:51 +0200

    TCP: large payload from server side, small buffer on the client

---
+++ n3t/read_buffer_test.go
@@ -0,0 +1,55 @@
+package n3t
+
+import (
+	"crypto/rand"
+	"io"
+	"net"
+	"testing"
+)
+
+func TestReadBuffer(t *testing.T) {
+	payload := make([]byte, 2<<24)
+	_, err := rand.Read(payload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Log(err)
+			return
+		}
+		defer conn.Close()
+
+		_, err = conn.Write(payload)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	conn, err := net.Dial("tcp", listener.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 1<<19)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				t.Error(err)
+			}
+			break
+		}
+		t.Logf("read %d bytes", n)
+	}
+
+	conn.Close()
+}
