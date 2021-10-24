f4f94b3b9e042696785281c612b8b365637f8385
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-10-24 08:19:58 +0200

    TLV: binary type - providing ReadFrom

--- types/binary.go
+++ types/binary.go
@@ -31,3 +31,28 @@ func (b Binary) WriteTo(w io.Writer) (int64, error) {
 	o, err := w.Write(b)
 	return n + int64(o), err
 }
+
+func (b Binary) ReadFrom(r io.Reader) (int64, error) {
+	var payloadType uint8
+	err := binary.Read(r, binary.BigEndian, &payloadType)
+	if err != nil {
+		return 0, err
+	}
+	var n int64 = 1
+
+	var size uint32
+	err = binary.Read(r, binary.BigEndian, &size)
+	if err != nil {
+		return n, err
+	}
+	n += 4
+
+	if size > MaxPayloadSize {
+		return n, ErrMaxPayloadSize
+	}
+
+	p := make([]byte, size)
+	o, err := r.Read(p)
+
+	return n + int64(o), err
+}
