7a724cb740922f281add3aa09120bc4cce7eab17
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2021-12-05 08:07:37 +0100

    Crypto: checksums MD5/SHA256 on multiple files

--- cmd/crypto.go
+++ cmd/crypto.go
@@ -4,9 +4,13 @@ import (
 	"crypto/md5"
 	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
 
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
+
+var Filename string
 
 func init() {
 	cryptoCmd.AddCommand(
@@ -26,32 +30,46 @@ var cryptoCmd = &cobra.Command{
 
 var cryptoMD5Cmd = &cobra.Command{
 	Use:     "md5",
-	Example: "md5 example_text",
-	Short:   "md5 hash function on first argument",
-	Long:    "md5 hash function on first argument",
+	Example: "md5 file_path ...",
+	Short:   "md5 hash function on files in arguments",
+	Long:    "md5 hash function on files in arguments",
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
+		if len(args) == 0 {
 			cmd.Help()
 			return
 		}
 
-		text := []byte(args[0])
-		fmt.Printf("%x\n", md5.Sum(text))
+		for _, filename := range args {
+			file, err := ioutil.ReadFile(filename)
+			if err != nil {
+				log.Info().Err(err).Str("filename", filename).Msgf("Error reading file!")
+				continue
+			}
+
+			fmt.Printf("%s\tMD5: %x\n", filename, md5.Sum(file))
+		}
 	},
 }
 
 var cryptoSHA256Cmd = &cobra.Command{
 	Use:     "sha256",
 	Example: "sha256 example_text",
-	Short:   "sha256 hash function on first argument",
-	Long:    "sha256 hash function on first argument",
+	Short:   "sha256 hash function on files in arguments",
+	Long:    "sha256 hash function on files in arguments",
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
+		if len(args) == 0 {
 			cmd.Help()
 			return
 		}
 
-		text := []byte(args[0])
-		fmt.Printf("%x\n", sha256.Sum256(text))
+		for _, filename := range args {
+			file, err := ioutil.ReadFile(filename)
+			if err != nil {
+				log.Info().Err(err).Str("filename", filename).Msgf("Error reading file!")
+				continue
+			}
+
+			fmt.Printf("%s\tSHA256: %x\n", filename, sha256.Sum256(file))
+		}
 	},
 }
