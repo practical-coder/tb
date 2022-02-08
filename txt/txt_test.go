17eaf4bc64a244db9b65b6f74348419fa291357a
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2022-02-08 20:35:53 +0100

    txt: simple count - basic tests

---
+++ txt/txt_test.go
@@ -0,0 +1,33 @@
+package txt
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestLines(t *testing.T) {
+	buff := bytes.NewBufferString("line1\nline2\nline3\nline4\nline5")
+	expected := 5
+	actual := countLines(buff)
+	if actual != expected {
+		t.Errorf("countLines Error actual: %d expected: %d", actual, expected)
+	}
+}
+
+func TestWords(t *testing.T) {
+	buff := bytes.NewBufferString("word1 word2 word3 word4 word5 word6\n")
+	expected := 6
+	actual := countWords(buff)
+	if actual != expected {
+		t.Errorf("countWords Error actual: %d expected: %d", actual, expected)
+	}
+}
+
+func TestBytes(t *testing.T) {
+	buff := bytes.NewBufferString("0123456789")
+	expected := 10
+	actual := countBytes(buff)
+	if actual != expected {
+		t.Errorf("countBytes Error actual: %d expected: %d", actual, expected)
+	}
+}
