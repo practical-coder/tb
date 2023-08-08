2ae3ed65c7d9ecbf65fa68acea11aaa4d756ab51
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2023-08-08 12:11:38 +0200

    Websockets: serve CPU temperature readings

--- ws/ws.go
+++ ws/ws.go
@@ -1,77 +1,21 @@
 package ws
 
 import (
-	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/rs/zerolog/log"
-
 	"golang.org/x/net/websocket"
 )
 
-func EchoServer() {
-	http.Handle("/", websocket.Handler(Echo))
+func StartServer() {
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/", fs)
+	http.Handle("/echo", websocket.Handler(Echo))
+	http.Handle("/cputemp", websocket.Handler(Temperature))
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
 		log.Fatal().
 			Err(err).
 			Msg("HTTP server error")
-	}
-}
-
-func Echo(conn *websocket.Conn) {
-	fmt.Println("Echoing")
-	for n := 0; n < 10; n++ {
-		msg := fmt.Sprintf("Hello %c\n", n+48)
-		fmt.Println("Sending to client: " + msg)
-		err := websocket.Message.Send(conn, msg)
-		if err != nil {
-			log.Fatal().
-				Err(err).
-				Msg("websocket message send error")
-			break
-		}
-		var reply string
-		err = websocket.Message.Receive(conn, &reply)
-		if err != nil {
-			log.Fatal().
-				Err(err).
-				Msg("websocket message receive error")
-			break
-		}
-
-		fmt.Println("Received back from client: " + reply)
-	}
-}
-
-func EchoClient(source string) {
-	conn, err := websocket.Dial(source, "", "http://localhost:12345")
-	if err != nil {
-		log.Fatal().
-			Err(err).
-			Str("source", source).
-			Msg("websocket Dial error")
-	}
-	var msg string
-	for {
-		err := websocket.Message.Receive(conn, &msg)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Info().
-				Err(err).
-				Msg("websocket message receive error")
-			break
-		}
-		fmt.Println("Received from server:", msg)
-		err = websocket.Message.Send(conn, msg)
-		if err != nil {
-			log.Info().
-				Err(err).
-				Msg("websocket message send error")
-			break
-		}
 	}
 }
